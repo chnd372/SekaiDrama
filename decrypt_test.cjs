607dcefb63897fac51366b0c29ffda80de03d9ea
chnd372 <chandratri372@gmail.com>
2026-09-07 07:47:18 +0000

    chore: remove test artifacts from repo

--- decrypt_test.cjs
+++
@@ -1,5 +0,0 @@
-const CryptoJS = require("crypto-js");
-const fs = require("fs");
-const s = CryptoJS.AES.decrypt(fs.readFileSync("/opt/data/SekaiDrama/enc.txt","utf8").trim(), "Sansekai-SekaiDrama").toString(CryptoJS.enc.Utf8);
-const j = JSON.parse(s);
-console.log(j.qualities[0].streamUrl);
